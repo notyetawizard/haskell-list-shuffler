fe04fe0b631aa75fe9ece509a852d98a5f131e10
notyetawizard <notyetawizard@gmail.com>
2015-11-29 20:32:23 -0600

    shuffle function took me about three hours but it works.

--- notyetagardener.hs
+++ notyetagardener.hs
@@ -1,24 +1,14 @@
 import System.Random
 
---mkStdGen 20151129
+--let seed = mkStdGen 098234507
+--in ghci to test things
 
-{-
-    Shuffle list function:
-    used to randomly order which cells are up next for growth
-
-    Steps:
-    pass function two lists (probably one empty) and a StdGen
-    get the length of the list being shuffled
-    pick a number between (0 or 1)? and it
-    append that element to the new list
-    call recursively until shuffled list is empty
-    return new list and StdGen 
--}
-
+--Pull items randomly out of the first list and place them
+--at the head of the second. Return with new RandomGen
 shuffle :: RandomGen g => ([a],[a]) -> g -> ([a],g)
 shuffle ([],b) g = (b, g)
 shuffle (a,b) g = 
-    --get random position in a, and the new RandomGen
+    --get random position in a, and a new RandomGen
     let (n, ng) = randomR (0,length a -1) g
     --split a, move element to b, put a back together
         (a1, a2) = splitAt n a
